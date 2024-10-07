9d6a6a6e90cddd606a4b7986909a8282fcdaed76
hazelmendoza <hazelannemendoza321@gmail.com>
2024-10-07 22:22:13 +0800

    add firebase setup

---
+++ libtrack/src/pages/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div>
+      <p className="text-2xl">Welcome to LibTrack</p>
+      
+      <img
+        src="/libtrack-logo.png"
+        alt="LibTrack Logo"
+        className="w-80 h-auto rounded-lg shadow-lg"
+      />
+    </div>
+  );
+}
+
+export default App;
